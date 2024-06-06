e38db2922f449783151314b98e400f27fc943363
Breno Saraiva <brenodev.saraiva@gmail.com>
2024-06-06 00:29:43 -0300

    first commit

---
+++ src/modules/home/page/index.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export { Home };
---
+++ src/shared/mock/selectPraias.ts
@@ -0,0 +1,182 @@
+export const praias: {value:string,label: string}[] = [
+    {
+      value: "-23.9948*-46.2569/Guarujá - Praia de Pitangueiras",
+      label: "Guarujá - Praia de Pitangueiras"
+    },
+    {
+      value: "-23.9883*-46.2414/Guarujá - Praia da Enseada",
+      label: "Guarujá - Praia da Enseada"
+    },
+    {
+      value: "-24.0126*-46.2866/Guarujá - Praia do Tombo",
+      label: "Guarujá - Praia do Tombo"
+    },
+    {
+      value: "-23.7853*-45.5738/São Sebastião - Praia de Maresias",
+      label: "São Sebastião - Praia de Maresias"
+    },
+    {
+      value: "-23.7224*-45.7292/São Sebastião - Praia de Juquehy",
+      label: "São Sebastião - Praia de Juquehy"
+    },
+    {
+      value: "-23.7965*-45.5983/São Sebastião - Praia de Camburi",
+      label: "São Sebastião - Praia de Camburi"
+    },
+    {
+      value: "-23.7547*-45.6333/São Sebastião - Praia da Baleia",
+      label: "São Sebastião - Praia da Baleia"
+    },
+    {
+      value: "-23.7862*-45.5833/São Sebastião - Praia de Boiçucanga",
+      label: "São Sebastião - Praia de Boiçucanga"
+    },
+    {
+      value: "-23.7554*-45.8467/São Sebastião - Praia de Barra do Una",
+      label: "São Sebastião - Praia de Barra do Una"
+    },
+    {
+      value: "-23.8342*-45.4134/São Sebastião - Praia de Barequeçaba",
+      label: "São Sebastião - Praia de Barequeçaba"
+    },
+    {
+      value: "-23.8111*-45.4973/São Sebastião - Praia de Toque-Toque Pequeno",
+      label: "São Sebastião - Praia de Toque-Toque Pequeno"
+    },
+    {
+      value: "-23.8052*-45.5145/São Sebastião - Praia de Toque-Toque Grande",
+      label: "São Sebastião - Praia de Toque-Toque Grande"
+    },
+    {
+      value: "-23.7837*-45.5957/São Sebastião - Praia de Paúba",
+      label: "São Sebastião - Praia de Paúba"
+    },
+    {
+      value: "-24.0433*-45.3647/Ilhabela - Praia do Bonete",
+      label: "Ilhabela - Praia do Bonete"
+    },
+    {
+      value: "-23.8757*-45.3678/Ilhabela - Praia do Curral",
+      label: "Ilhabela - Praia do Curral"
+    },
+    {
+      value: "-23.8213*-45.3831/Ilhabela - Praia da Feiticeira",
+      label: "Ilhabela - Praia da Feiticeira"
+    },
+    {
+      value: "-23.8324*-45.3154/Ilhabela - Praia de Castelhanos",
+      label: "Ilhabela - Praia de Castelhanos"
+    },
+    {
+      value: "-23.7864*-45.3592/Ilhabela - Praia de Perequê",
+      label: "Ilhabela - Praia de Perequê"
+    },
+    {
+      value: "-23.8219*-45.3773/Ilhabela - Praia do Julião",
+      label: "Ilhabela - Praia do Julião"
+    },
+    {
+      value: "-23.8153*-45.3903/Ilhabela - Praia Grande",
+      label: "Ilhabela - Praia Grande"
+    },
+    {
+      value: "-23.7259*-45.3494/Ilhabela - Praia da Armação",
+      label: "Ilhabela - Praia da Armação"
+    },
+    {
+      value: "-23.7577*-45.3671/Ilhabela - Praia do Viana",
+      label: "Ilhabela - Praia do Viana"
+    },
+    {
+      value: "-23.7064*-45.3499/Ilhabela - Praia do Jabaquara",
+      label: "Ilhabela - Praia do Jabaquara"
+    },
+    {
+      value: "-23.7799*-45.6495/São Sebastião - Praia da Barra do Sahy",
+      label: "São Sebastião - Praia da Barra do Sahy"
+    },
+    {
+      value: "-23.5525*-45.2405/Caraguatatuba - Praia da Tabatinga",
+      label: "Caraguatatuba - Praia da Tabatinga"
+    },
+    {
+      value: "-23.5898*-45.2652/Caraguatatuba - Praia da Cocanha",
+      label: "Caraguatatuba - Praia da Cocanha"
+    },
+    {
+      value: "-23.6227*-45.3835/Caraguatatuba - Praia Martim de Sá",
+      label: "Caraguatatuba - Praia Martim de Sá"
+    },
+    {
+      value: "-23.6271*-45.4149/Caraguatatuba - Praia do Indaiá",
+      label: "Caraguatatuba - Praia do Indaiá"
+    },
+    {
+        value: "-23.5783*-45.2765/Caraguatatuba - Praia de Massaguaçu",
+        label: "Caraguatatuba - Praia de Massaguaçu"
+      },
+      {
+        value: "-23.5178*-45.1601/Ubatuba - Praia da Lagoinha",
+        label: "Ubatuba - Praia da Lagoinha"
+      },
+      {
+        value: "-23.5068*-45.1283/Ubatuba - Praia do Lázaro",
+        label: "Ubatuba - Praia do Lázaro"
+      },
+      {
+        value: "-23.4881*-45.0941/Ubatuba - Praia da Enseada",
+        label: "Ubatuba - Praia da Enseada"
+      },
+      {
+        value: "-23.4232*-45.0651/Ubatuba - Praia do Perequê-Açu",
+        label: "Ubatuba - Praia do Perequê-Açu"
+      },
+      {
+        value: "-23.4101*-45.0485/Ubatuba - Praia Vermelha do Norte",
+        label: "Ubatuba - Praia Vermelha do Norte"
+      },
+      {
+        value: "-23.3933*-45.0007/Ubatuba - Praia de Itamambuca",
+        label: "Ubatuba - Praia de Itamambuca"
+      },
+      {
+        value: "-23.3526*-44.9765/Ubatuba - Praia do Félix",
+        label: "Ubatuba - Praia do Félix"
+      },
+      {
+        value: "-23.3452*-44.9439/Ubatuba - Praia da Fazenda",
+        label: "Ubatuba - Praia da Fazenda"
+      },
+      {
+        value: "-23.4767*-45.0825/Ubatuba - Praia das Toninhas",
+        label: "Ubatuba - Praia das Toninhas"
+      },
+      {
+        value: "-23.4762*-45.0634/Ubatuba - Praia Grande",
+        label: "Ubatuba - Praia Grande"
+      },
+      {
+        value: "-23.5335*-45.1569/Ubatuba - Praia do Sapê",
+        label: "Ubatuba - Praia do Sapê"
+      },
+      {
+        value: "-23.4523*-45.0692/Ubatuba - Praia do Tenório",
+        label: "Ubatuba - Praia do Tenório"
+      },
+      {
+        value: "-23.4555*-45.0532/Ubatuba - Praia do Cedro",
+        label: "Ubatuba - Praia do Cedro"
+      },
+      {
+        value: "-23.4752*-45.0896/Ubatuba - Praia do Estaleiro",
+        label: "Ubatuba - Praia do Estaleiro"
+      },
+      {
+        value: "-23.3791*-44.9837/Ubatuba - Praia do Prumirim",
+        label: "Ubatuba - Praia do Prumirim"
+      },
+      {
+        value: "-23.4801*-45.1086/Ubatuba - Praia da Santa Rita",
+        label: "Ubatuba - Praia da Santa Rita"
+      }
+    ];
