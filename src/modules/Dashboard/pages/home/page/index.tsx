72a8c76fb4cb6c1745d439388d0384a88105e4a7
Breno Saraiva <brenodev.saraiva@gmail.com>
2024-06-07 20:07:20 -0300

    feat: get post in homePage

--- src/modules/Dashboard/pages/home/page/index.tsx
+++ src/modules/Dashboard/pages/home/page/index.tsx
@@ -1,27 +1,62 @@
+import { useEffect, useState } from "react";
 import { createPost } from "../../../../../api/createPost/createPost.service";
 import { createPostProp } from "../../../../../shared/types/createPost";
 import { FormDescription } from "../components/form";
 import { Posts } from "../components/posts";
+import { getCategorias } from "../../../../../api/getPost/getPost.service";
+
+type comentariosProps = {
+  nome_pessoa: string;
+  comentario: string;
+  coordenada: string;
+  endereco: string;
+  id: string;
+};
 
 function Home() {
+  const [comentarios, setComentarios] = useState<comentariosProps[]>([]);
+
   async function handleSubmit(data: createPostProp) {
     await createPost(data);
   }
 
+  async function getcomentarios() {
+    const list = await getCategorias();
+    setComentarios(list);
+  }
+
+  useEffect(() => {
+    getcomentarios();
+  }, []);
+  console.log("renderizou");
+
+  const marcadores: string[] = [];
+  comentarios.map((item) => {
+    marcadores.push(item.coordenada);
+  });
+
   return (
     <div className="grid grid-cols-3  gap-5 p-8">
-      <div className="bg-red-500 col-span-2 rounded-lg">
-        <img src="" alt="" />
+      <div className="col-span-2 rounded-lg flex justify-center items-center">
+        <img
+          src={`https://maps.googleapis.com/maps/api/staticmap?center=Santos,SP&zoom=7&size=700x450&markers=color:red%7C${marcadores.join(
+            "%7C"
+          )}&key=AIzaSyDHTihUvCtc9auitAv5P8dD_7Z1evwRJZI`}
+          alt="imgMaps"
+        />
       </div>
       <div className="col-span-1 col-start-3">
         <FormDescription handleOnSubmit={handleSubmit} />
       </div>
-      <div className="bg-yellow-200 col-span-2 rounded-md">
-        <Posts
-          Localização="Ceará"
-          name="Breno Saraiva"
-          description="um cara bacana bonito e sensual"
-        />
+      <div className="bg-[#8AB4F8] col-span-2 rounded-md">
+        {comentarios.length > 0 &&
+          comentarios.map((item) => (
+            <Posts
+              Localização={item.endereco}
+              name={item.nome_pessoa}
+              description={item.comentario}
+            />
+          ))}
       </div>
     </div>
   );
